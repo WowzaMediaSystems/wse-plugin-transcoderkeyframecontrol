95a3d0e998c8bb7392d45381cdb3b9ebab7d332e
Roger Littin <roger@wowza.com>
2018-02-23 14:13:58 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# TranscoderKeyFrameControl
+The **TranscoderKeyFrameControl** module for [Wowza Streaming Engine™ media server software](https://www.wowza.com/products/streaming-engine) can be used to insert key frames into the transcoded streams at regular intervals. It overrides the original key frame settings and will ensure that the keyframes are aligned between each rendition.
+
+## Prerequisites
+Wowza Streaming Engine 4.6.0 or later is required.
+
+Wowza Transcoder must be enabled in the Wowza Streaming Engine application.
+
+## Usage
+The following properties can be used to configure the module.
+Path | Name | Value | Comment
+--- | --- | --- | ---
+/Root/Application/ | transcoderGopInterval | 2000 | default 2000ms
+/Root/Application/ | transcoderGopIntervalDebugLog | true | default false
+
+The key frames are inserted each time the stream timecode rolls over the `transcoderGopInterval` value so if the transcoding starts on a non-zero timecode then the first GOP may be smaller than the `transcoderGopInterval` value.
+
+## More resources
+[Wowza Streaming Engine Server-Side API Reference](https://www.wowza.com/resources/WowzaStreamingEngine_ServerSideAPI.pdf)
+
+[How to extend Wowza Streaming Engine using the Wowza IDE](https://www.wowza.com/forums/content.php?759-How-to-extend-Wowza-Streaming-Engine-using-the-Wowza-IDE)
+
+Wowza Media Systems™ provides developers with a platform to create streaming applications and solutions. See [Wowza Developer Tools](https://www.wowza.com/resources/developers) to learn more about our APIs and SDK.
+
+To use the compiled version of this module, see [How to insert aligned key frames into Wowza Transcoder streams (TranscoderKeyFrameControl)](https://www.wowza.com/docs).
+
+## Contact
+[Wowza Media Systems, LLC](https://www.wowza.com/contact)
+
+## License
+This code is distributed under the [Wowza Public License](https://github.com/WowzaMediaSystems/wse-plugin-transcodertimedsnapshot/blob/master/LICENSE.txt).
+
+![alt tag](http://wowzalogs.com/stats/githubimage.php?plugin=wse-plugin-transcoderkeyframecontrol)
---
+++ src/com/wowza/plugin/transcoder/ModuleTranscoderVideoKeyFrameControl.java
@@ -0,0 +1,136 @@
+/**
+ * This code and all components (c) Copyright 2006 - 2018, Wowza Media Systems, LLC. All rights reserved.
+ * This code is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
+ */
+package com.wowza.plugin.transcoder;
+
+import java.util.List;
+
+import com.wowza.util.FLVUtils;
+import com.wowza.wms.application.IApplicationInstance;
+import com.wowza.wms.logging.WMSLogger;
+import com.wowza.wms.logging.WMSLoggerFactory;
+import com.wowza.wms.logging.WMSLoggerIDs;
+import com.wowza.wms.module.ModuleBase;
+import com.wowza.wms.stream.IMediaStream;
+import com.wowza.wms.stream.livetranscoder.ILiveStreamTranscoder;
+import com.wowza.wms.stream.livetranscoder.LiveStreamTranscoderNotifyBase;
+import com.wowza.wms.transcoder.model.LiveStreamTranscoder;
+import com.wowza.wms.transcoder.model.LiveStreamTranscoderActionNotifyBase;
+import com.wowza.wms.transcoder.model.TranscoderDecodedFrame;
+import com.wowza.wms.transcoder.model.TranscoderSession;
+import com.wowza.wms.transcoder.model.TranscoderSessionVideo;
+import com.wowza.wms.transcoder.model.TranscoderSessionVideoEncode;
+import com.wowza.wms.transcoder.model.TranscoderStream;
+import com.wowza.wms.transcoder.model.TranscoderStreamDestinationVideo;
+import com.wowza.wms.transcoder.model.TranscoderVideoEncodeFrameContext;
+import com.wowza.wms.transcoder.model.TranscoderVideoEncoderNotifyBase2;
+
+public class ModuleTranscoderVideoKeyFrameControl extends ModuleBase
+{
+	private static final String CLASSNAME = "ModuleTranscoderVideoKeyFrameControl";
+
+	private WMSLogger logger = null;
+	private IApplicationInstance appInstance = null;
+
+	class TranscoderCreateNotifier extends LiveStreamTranscoderNotifyBase
+	{
+		TranscoderActionNotifier transcoderActionNotifier = new TranscoderActionNotifier();
+
+		@Override
+		public void onLiveStreamTranscoderCreate(ILiveStreamTranscoder liveStreamTranscoder, IMediaStream stream)
+		{
+			logger.info(CLASSNAME + "#TranscoderCreateNotifier.onLiveStreamTranscoderCreate[" + appInstance.getContextStr() + "]: " + stream.getName(), WMSLoggerIDs.CAT_application, WMSLoggerIDs.EVT_comment);
+
+			// Add a live stream transcoder action listener so we can plug into the transcoding pipeline
+			((LiveStreamTranscoder)liveStreamTranscoder).addActionListener(transcoderActionNotifier);
+		}
+	}
+
+	class TranscoderActionNotifier extends LiveStreamTranscoderActionNotifyBase
+	{
+
+		// Called when the transcoding session is setup and ready to start
+		@Override
+		public void onInitStop(LiveStreamTranscoder liveStreamTranscoder)
+		{
+			logger.info(CLASSNAME + "#TranscoderActionNotifier.onInitStop[" + appInstance.getContextStr() + "/" + liveStreamTranscoder.getStreamName() + "]", WMSLoggerIDs.CAT_application, WMSLoggerIDs.EVT_comment);
+
+			try
+			{
+				while (true)
+				{
+					TranscoderStream transcoderStream = liveStreamTranscoder.getTranscodingStream();
+					if (transcoderStream == null)
+						break;
+
+					TranscoderSession transcoderSession = liveStreamTranscoder.getTranscodingSession();
+					TranscoderSessionVideo transcoderVideoSession = transcoderSession.getSessionVideo();
+
+					List<TranscoderSessionVideoEncode> videoEncodes = transcoderVideoSession.getEncodes();
+					for (TranscoderSessionVideoEncode videoEncode : videoEncodes)
+					{
+						logger.info(CLASSNAME + "#TranscoderActionNotifier.onInitStop[" + appInstance.getContextStr() + "/" + liveStreamTranscoder.getStreamName() + "] Add frame listener: rendition:" + videoEncode.getName(), WMSLoggerIDs.CAT_application, WMSLoggerIDs.EVT_comment);
+
+						// add a transcoder video frame listener
+						videoEncode.addFrameListener(new TranscoderVideoEncoderNotifier(liveStreamTranscoder));
+					}
+					break;
+				}
+			}
+			catch (Exception e)
+			{
+				logger.error(CLASSNAME + "#TranscoderActionNotifier.onInitStop[" + appInstance.getContextStr() + "/" + liveStreamTranscoder.getStreamName() + "]  ", e);
+			}
+		}
+	}
+
+	class TranscoderVideoEncoderNotifier extends TranscoderVideoEncoderNotifyBase2
+	{
+		private LiveStreamTranscoder liveStreamTranscoder = null;
+		private boolean debugLog = false;
+		private long gopStartTimecode = -1;
+		private long gopInterval = 2000;
+
+		public TranscoderVideoEncoderNotifier(LiveStreamTranscoder liveStreamTranscoder)
+		{
+			this.liveStreamTranscoder = liveStreamTranscoder;
+			debugLog = appInstance.getProperties().getPropertyBoolean("transcoderGopIntervalDebugLog", debugLog);
+			gopInterval = appInstance.getProperties().getPropertyLong("transcoderGopInterval", gopInterval);
+		}
+
+		@Override
+		public void onBeforeEncodeFrame(TranscoderSessionVideoEncode sessionVideoEncode, TranscoderStreamDestinationVideo destinationVideo, TranscoderVideoEncodeFrameContext frameContext)
+		{
+			TranscoderDecodedFrame decodedFrame = frameContext.getFrameHolder().getEncoderNextFrame();
+			if (decodedFrame != null)
+			{
+				int frameType = FLVUtils.FLV_PFRAME;
+				long frameTimecode = decodedFrame.getTimecode();
+
+				// every gopInterval we are going to force a key frame
+				if (frameTimecode >= gopStartTimecode)
+				{
+					frameType = FLVUtils.FLV_KFRAME;
+					if (debugLog)
+						logger.info(CLASSNAME + "#TranscoderVideoEncoderNotifier.onBeforeEncodeFrame[" + appInstance.getContextStr() + "/" + liveStreamTranscoder.getStreamName() + "(" + destinationVideo.getDestination().getName() + ")" + "]  Insert key frame: gopStartTimecode:" + gopStartTimecode
+								+ " timecode:" + frameTimecode, WMSLoggerIDs.CAT_application, WMSLoggerIDs.EVT_comment);
+
+					gopStartTimecode = ((frameTimecode / gopInterval) + 1) * gopInterval;
+				}
+
+				frameContext.setFrameType(frameType);
+			}
+		}
+	}
+
+	public void onAppStart(IApplicationInstance appInstance)
+	{
+		logger = WMSLoggerFactory.getLoggerObj(appInstance);
+		logger.info(CLASSNAME + ".onAppStart[" + appInstance.getContextStr() + "]: Build #1.", WMSLoggerIDs.CAT_application, WMSLoggerIDs.EVT_comment);
+
+		this.appInstance = appInstance;
+
+		appInstance.addLiveStreamTranscoderListener(new TranscoderCreateNotifier());
+	}
+}
